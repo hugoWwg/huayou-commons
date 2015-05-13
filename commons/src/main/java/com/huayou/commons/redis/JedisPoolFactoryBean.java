ba601712277d288f6dc145ca98b4dccab07111cf
hugoWwg <hugo_wwg@163.com>
2015-05-13 09:32:33 +0800

    执行idle chicking

--- commons/src/main/java/com/huayou/commons/redis/JedisPoolFactoryBean.java
+++ commons/src/main/java/com/huayou/commons/redis/JedisPoolFactoryBean.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.FactoryBean;
+
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
@@ -21,6 +22,8 @@ public class JedisPoolFactoryBean implements FactoryBean<JedisPool> {
     private String password;
     private int database;
     private int threadCount;
+    private int checkingIntervalSecs;
+    private int idleTimeSecs;
 
     public String getHost() {
         return host;
@@ -70,20 +73,45 @@ public class JedisPoolFactoryBean implements FactoryBean<JedisPool> {
         this.threadCount = threadCount;
     }
 
+    public int getCheckingIntervalSecs() {
+        return checkingIntervalSecs;
+    }
+
+    public void setCheckingIntervalSecs(int checkingIntervalSecs) {
+        this.checkingIntervalSecs = checkingIntervalSecs;
+    }
+
+    public int getIdleTimeSecs() {
+        return idleTimeSecs;
+    }
+
+    public void setIdleTimeSecs(int idleTimeSecs) {
+        this.idleTimeSecs = idleTimeSecs;
+    }
+
     @Override
     public JedisPool getObject() throws Exception {
-        // 设置Pool大小，设为与线程数等大，并屏蔽掉idle checking
-        JedisPoolConfig poolConfig = JedisUtils.createPoolConfig(threadCount, threadCount);
+        // 设置Pool大小，设为与线程数等大，执行idle checking
+        JedisPoolConfig poolConfig = JedisUtils.createPoolConfig(threadCount, threadCount,
+                                                                 checkingIntervalSecs,
+                                                                 idleTimeSecs);
+        // make sure borrow a jedis available
+        poolConfig.setTestOnBorrow(true);
+        // 扫描idle，如果validate失败，则会在pool中drop。
+        poolConfig.setTestWhileIdle(true);
         // create jedis pool
         JedisPool jedisPool = new JedisPool(poolConfig, host, port, timeout, password, database);
-        logger.info("create jedisPool[{}] ,host[{}] port[{}] timeout[{}] threadCount[{}] password[{}] database[{}]",
-                ToStringBuilder.reflectionToString(jedisPool),
-                host,
-                port,
-                timeout,
-                threadCount,
-                password,
-                database);
+        logger.info(
+            "create jedisPool[{}] ,host[{}] port[{}] timeout[{}] threadCount[{}] password[{}] database[{}]",
+            ToStringBuilder.reflectionToString(jedisPool),
+            host,
+            port,
+            timeout,
+            threadCount,
+            password,
+            database,
+            checkingIntervalSecs,
+            idleTimeSecs);
         return jedisPool;
     }
 
